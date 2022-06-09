818f7040e4e8ccd3e77b3e9d4946fb910e951df4
DaniilMaeve <107078460+DaniilMaeve@users.noreply.github.com>
2022-06-09 09:57:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slayder').slick({
+      arrows:false,
+      dots:true,
+      adaptiveHeight:true,
+      speed:1000,
+      autoplay:true,    
+      autoplaySpeed:3000,
+      TouchTreashold:5,
+      slidesToShow:1
+  });
+});
